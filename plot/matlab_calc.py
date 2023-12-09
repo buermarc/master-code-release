b78ce364dfba36e1d54fe8d4bc14be618153c45b
buermarc <buermarc2@gmail.com>
2023-12-09 10:40:51 +0100

    Update plot matlab_calc with python3.7 deps

--- plot/matlab_calc.py
+++ plot/matlab_calc.py
@@ -7,29 +7,40 @@ constrained_joint_groups = [ [ 18, 19, 20 ], [ 22, 23, 24 ], [ 5, 6, 7 ], [ 12, 
 
 matlab = pandas.read_csv("../_matlab/file1.csv")
 cpp = pandas.read_csv("../data/out.csv")
-unfiltered = pandas.read_csv("../data/unfiltered_out.csv")
+unfiltered = pandas.read_csv("../data/un_out.csv")
+
+matlab_vel = pandas.read_csv("../_matlab/file2.csv")
+cpp_vel = pandas.read_csv("../data/vel_out.csv")
+timestamps = np.load("../data/timestamps.npy")
+
+rows = unfiltered.values.shape[0]-1
+cols = unfiltered.values.shape[1]
+breakpoint()
+finite_diff_vel = np.zeros_like(cpp_vel)
+finite_diff_vel[1:] = (unfiltered.values[1:] - unfiltered.values[:-1]) / (timestamps[1:] - timestamps[:-1]).repeat(cols).reshape(rows, cols)
+finite_diff_vel[0] = finite_diff_vel[1]
 
 for joint_idx in range(32):
     fig, axis = plt.subplots(3, 2)
-    mat_values = matlab[matlab.columns[(3 * joint_idx) : (3 * (joint_idx+1))]]
+    mat_values = matlab[matlab.columns[(3 * joint_idx) : (3 * (joint_idx+1))]].values
 
     fig.tight_layout() # Or equivalently,  "plt.tight_layout()"
 
     axis[0, 0].plot(mat_values)
     axis[0, 0].set_title(f"Matlab {joint_idx}")
 
-    values = cpp[cpp.columns[(3 * joint_idx) : (3 * (joint_idx+1))]]
+    values = cpp[cpp.columns[(3 * joint_idx) : (3 * (joint_idx+1))]].values
 
     result = mat_values - values
 
     axis[1, 0].plot(values)
     axis[1, 0].set_title(f"Cpp {joint_idx}")
 
-    un_values = unfiltered[unfiltered.columns[(3 * joint_idx) : (3 * (joint_idx+1))]]
+    un_values = unfiltered[unfiltered.columns[(3 * joint_idx) : (3 * (joint_idx+1))]].values
     axis[2, 0].plot(un_values)
     axis[2, 0].set_title(f"Unfiltered {joint_idx}")
 
-    axis[0, 1].plot(result[result.columns[:]], marker=",", alpha=0.5)
+    axis[0, 1].plot(result, marker=",", alpha=0.5)
     axis[0, 1].set_title(f"Diff Cpp - Mat {joint_idx}")
 
     axis[1, 1].plot(values - un_values, marker=",", alpha=0.5)
@@ -42,6 +53,48 @@ for joint_idx in range(32):
     plt.close()
 
 plt.cla()
+
+for joint_idx in range(32):
+    fig, axis = plt.subplots(3, 2)
+    mat_values = matlab_vel[matlab_vel.columns[(3 * joint_idx) : (3 * (joint_idx+1))]].values
+
+    fig.tight_layout() # Or equivalently,  "plt.tight_layout()"
+
+    axis[0, 0].plot(mat_values)
+    axis[0, 0].set_title(f"Vel Matlab {joint_idx}")
+
+    values = cpp_vel[cpp_vel.columns[(3 * joint_idx) : (3 * (joint_idx+1))]].values
+
+    result = mat_values - values
+
+    axis[1, 0].plot(values)
+    axis[1, 0].set_title(f"Vel Cpp {joint_idx}")
+
+    '''
+    un_values = unfiltered[unfiltered.columns[(3 * joint_idx) : (3 * (joint_idx+1))]]
+    axis[2, 0].plot(un_values)
+    axis[2, 0].set_title(f"Unfiltered {joint_idx}")
+    '''
+
+    axis[0, 1].plot(result, marker=",", alpha=0.5)
+    axis[0, 1].set_title(f"Vel Diff Cpp - Mat {joint_idx}")
+
+    '''
+    axis[1, 1].plot(values - un_values, marker=",", alpha=0.5)
+    axis[1, 1].set_title(f"Diff Cpp - Unfiltered {joint_idx}")
+
+    axis[2, 1].plot(mat_values - un_values, marker=",", alpha=0.5)
+    axis[2, 1].set_title(f"Diff Matlab - Unfiltered {joint_idx}")
+    '''
+
+    axis[1, 1].plot(finite_diff_vel[:, (3 * joint_idx) : (3 * (joint_idx+1))], marker=",", alpha=0.5)
+    axis[1, 1].set_title(f"Finite diff vel {joint_idx}")
+
+    plt.savefig(f"results/vel-out-{joint_idx}.pdf")
+    plt.close()
+
+plt.cla()
+
 
 for group in constrained_joint_groups:
     fig, axis = plt.subplots(1, 2)
