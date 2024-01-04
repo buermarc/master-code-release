49d4ac162658ae637a2b3d412c6361d957a95b1b
buermarc <buermarc2@gmail.com>
2024-01-04 22:54:11 +0100

    Read all different tsv files for a recording

--- src/read_tsv.cpp
+++ src/read_tsv.cpp
@@ -1,10 +1,24 @@
 #include<string>
+#include<tuple>
+#include<algorithm>
 #include<vector>
 #include<fstream>
 #include<iostream>
 #include<sstream>
 #include<tclap/CmdLine.h>
 #include<filter/Point.hpp>
+
+template<typename T>
+void print_vec(std::vector<T> vector) {
+    std::for_each(vector.cbegin(), vector.cend()-1, [](auto ele){ std::cout << ele << ", " << std::endl;});
+    std::cout << vector.back() << std::endl;
+}
+template<typename T>
+void print_vec(std::string name, std::vector<T> vector) {
+    std::cout << "@name: ";
+    std::for_each(vector.cbegin(), vector.cend()-1, [](auto ele){ std::cout << ele << ", " << std::endl;});
+    std::cout << vector.back() << std::endl;
+}
 
 std::string trimString(std::string str)
 {
@@ -53,7 +67,7 @@ void read_marker_file(std::string file) {
             std::cout << element << " ";
         }
         std::cout << std::endl;;
-    } while (key != "TRAJECTORY_TYPES");
+    } while (key != "Frame");
 
 
 
@@ -153,17 +167,28 @@ void read_marker_file(std::string file) {
             )
         );
     }
+
+    print_vec("timestamps", timestamps);
+    print_vec("l_sae", l_sae);
+    print_vec("l_hle", l_hle);
+    print_vec("l_usp", l_usp);
+    print_vec("r_hle", r_hle);
+    print_vec("r_usp", r_usp);
 }
 
-void read_force_files(std::string force_file_f_1, std::string force_file_f_2) {
+std::tuple<
+    std::vector<double>,
+    std::vector<Point<double>>,
+    std::vector<Point<double>>,
+    std::vector<Point<double>>
+>
+read_force_plate_file(std::string force_plate_file) {
+    std::ifstream csv_file(force_plate_file);
 
-    std::ifstream csv_file_1(force_file_f_1);
-    std::ifstream csv_file_2(force_file_f_2);
-
-    // Go through headers
+    // Go through headers for file
     std::string key = "";
     do {
-        auto header = getNextLineAndSplitIntoTokens(csv_file_1);
+        auto header = getNextLineAndSplitIntoTokens(csv_file);
         if (header.size() > 0) {
             key = header.at(0);
         }
@@ -171,25 +196,17 @@ void read_force_files(std::string force_file_f_1, std::string force_file_f_2) {
             std::cout << element << " ";
         }
         std::cout << std::endl;;
-    } while (key != "TRAJECTORY_TYPES");
+    } while (key != "SAMPLE");
 
 
 
     std::vector<double> timestamps;
-    std::vector<Point<double>> l_ak;
-    std::vector<Point<double>> r_ak;
-    std::vector<Point<double>> b_ak;
+    std::vector<Point<double>> force;
+    std::vector<Point<double>> moment;
+    std::vector<Point<double>> cop;
 
-    std::vector<Point<double>> l_sae;
-
-    std::vector<Point<double>> l_hle;
-    std::vector<Point<double>> l_usp;
-
-    std::vector<Point<double>> r_hle;
-    std::vector<Point<double>> r_usp;
-
-    while (!csv_file_1.eof()) {
-        auto results = getNextLineAndSplitIntoTokens(csv_file_1);
+    while (!csv_file.eof()) {
+        auto results = getNextLineAndSplitIntoTokens(csv_file);
         if (results.size() == 1) {
             for (auto e : results) {
                 std::cout << e << std::endl;
@@ -200,7 +217,7 @@ void read_force_files(std::string force_file_f_1, std::string force_file_f_2) {
         int i;
 
         i = 2;
-        l_ak.push_back(
+        force.push_back(
             Point<double>(
                 std::stod(results.at(i + 0)),
                 std::stod(results.at(i + 1)),
@@ -209,7 +226,7 @@ void read_force_files(std::string force_file_f_1, std::string force_file_f_2) {
         );
 
         i += 3;
-        r_ak.push_back(
+        moment.push_back(
             Point<double>(
                 std::stod(results.at(i + 0)),
                 std::stod(results.at(i + 1)),
@@ -218,52 +235,7 @@ void read_force_files(std::string force_file_f_1, std::string force_file_f_2) {
         );
 
         i += 3;
-        b_ak.push_back(
-            Point<double>(
-                std::stod(results.at(i + 0)),
-                std::stod(results.at(i + 1)),
-                std::stod(results.at(i + 2))
-            )
-        );
-
-        i += 3;
-        l_sae.push_back(
-            Point<double>(
-                std::stod(results.at(i + 0)),
-                std::stod(results.at(i + 1)),
-                std::stod(results.at(i + 2))
-            )
-        );
-
-        i += 3;
-        l_hle.push_back(
-            Point<double>(
-                std::stod(results.at(i + 0)),
-                std::stod(results.at(i + 1)),
-                std::stod(results.at(i + 2))
-            )
-        );
-
-        i += 3;
-        l_usp.push_back(
-            Point<double>(
-                std::stod(results.at(i + 0)),
-                std::stod(results.at(i + 1)),
-                std::stod(results.at(i + 2))
-            )
-        );
-
-        i += 3;
-        r_hle.push_back(
-            Point<double>(
-                std::stod(results.at(i + 0)),
-                std::stod(results.at(i + 1)),
-                std::stod(results.at(i + 2))
-            )
-        );
-
-        i += 3;
-        r_usp.push_back(
+        cop.push_back(
             Point<double>(
                 std::stod(results.at(i + 0)),
                 std::stod(results.at(i + 1)),
@@ -271,6 +243,21 @@ void read_force_files(std::string force_file_f_1, std::string force_file_f_2) {
             )
         );
     }
+    return std::make_tuple(timestamps, force, moment, cop);
+}
+
+void read_force_plate_files(std::string force_plate_file_f1, std::string force_plate_file_f2) {
+    auto [timestamp_f1, force_f1, moment_f1, com_f1] = read_force_plate_file(force_plate_file_f1);
+    auto [timestamp_f2, force_f2, moment_f2, com_f2] = read_force_plate_file(force_plate_file_f2);
+
+    print_vec("timestamp_f1", timestamp_f1);
+    print_vec("force_f1", force_f1);
+    print_vec("moment_f1", moment_f1);
+    print_vec("com_f1", com_f1);
+    print_vec("timestamp_f2", timestamp_f2);
+    print_vec("force_f2", force_f2);
+    print_vec("moment_f2", moment_f2);
+    print_vec("com_f2", com_f2);
 }
 
 int main(int argc, char** argv) {
@@ -287,17 +274,18 @@ int main(int argc, char** argv) {
 
     file.replace(file.find(".tsv"), sizeof(".tsv") - 1, "");
     std::stringstream marker_file_name;
-    std::stringstream force_file_name_f_1;
-    std::stringstream force_file_name_f_2;
+    std::stringstream force_plate_file_name_f1;
+    std::stringstream force_plate_file_name_f2;
 
     marker_file_name << file <<  ".tsv";
-    force_file_name_f_1 << file << "_f_1.tsv";
-    force_file_name_f_2 << file << "_f_2.tsv";
+    force_plate_file_name_f1 << file << "_f_1.tsv";
+    force_plate_file_name_f2 << file << "_f_2.tsv";
 
     read_marker_file(marker_file_name.str());
+    read_force_plate_files(force_plate_file_name_f1.str(), force_plate_file_name_f2.str());
 
     std::cout << marker_file_name.str() << std::endl;
-    std::cout << force_file_name_f_1.str() << std::endl;
-    std::cout << force_file_name_f_2.str() << std::endl;
+    std::cout << force_plate_file_name_f1.str() << std::endl;
+    std::cout << force_plate_file_name_f2.str() << std::endl;
 
 }
