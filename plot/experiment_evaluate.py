f93f5c331f96c0ab2ccdcd06ecc912320842c6fb
buermarc <buermarc2@gmail.com>
2024-01-31 16:53:06 +0100

    Script to load experiment evaluated data and further evaluate them

---
+++ plot/experiment_evaluate.py
@@ -0,0 +1,27 @@
+import numpy as np
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("experiment_folder")
+
+    args = parser.parse_args()
+
+    truth = np.load(f"{args.experiment_folder}/truth.npy")
+    filtered = np.load(f"{args.experiment_folder}/filtered.npy")
+    unfiltered = np.load(f"{args.experiment_folder}/unfiltered.npy")
+
+    error_filtered = np.linalg.norm(truth - filtered, axis=2)
+    rms_filtered = np.sqrt(np.mean(np.power(error_filtered, 2), axis=0))
+
+    error_unfiltered = np.linalg.norm(truth - unfiltered, axis=2)
+    rms_unfiltered = np.sqrt(np.mean(np.power(error_unfiltered, 2), axis=0))
+
+    print(f"rms_filtered: {rms_filtered}")
+    print(f"rms_unfiltered: {rms_unfiltered}")
+    print(f"diff rms: {(rms_unfiltered - rms_filtered)}")
+
+
+if __name__ == "__main__":
+    main()
