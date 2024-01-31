c4e510701b3420d86711ddc5751b9501fa565330
buermarc <buermarc2@gmail.com>
2024-01-31 22:42:33 +0100

    Add more steps to grid search for simulation eval

--- grid_search_evaluation.sh
+++ grid_search_evaluation.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p ./plot/results/simulation
-for i in $(python -c 'import numpy as np; [print(i) for i in np.arange(0, 0.0016, 0.0001)]'); do
+for i in $(python -c 'import numpy as np; [print(i) for i in np.arange(0, 0.0016, 0.00001)]'); do
     ./build/load $i
     cd plot
     python simulation_evaluate.py > results/simulation/${i}.log
