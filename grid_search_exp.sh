9d167d58219e9535933722a4495eb08466e4c557
buermarc <buermarc2@gmail.com>
2024-02-02 19:14:47 +0100

    More experimenet evaluate plot and gird search adaption for exp

--- grid_search_exp.sh
+++ grid_search_exp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p /tmp/results
-for i in $(python -c 'import numpy as np; [print(i) for i in np.arange(0, 16, 1)]'); do
+for i in $(python -c 'import numpy as np; [print(i) for i in np.arange(5, 11, 1)]'); do
     cd /home/orb/repos/kinect_mocap_studio/
     JSON=$(./build/kinect_mocap_studio -i output_NFOV_UNBINNED_OFF_30fps_122.mkv | rg 'Frames written' | awk '{print$5}')
     echo "{\"qtm_file\": \"/home/orb/Desktop/Azure_Kinect_Marcus_Filipp_Edited/S20003.tsv\", \"kinect_file\": \"$JSON\"}" > ./experiments/S20003.json
--- plot/experiment_evaluate.py
+++ plot/experiment_evaluate.py
@@ -1,5 +1,6 @@
 import numpy as np
 import argparse
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -22,6 +23,36 @@ def main():
     print(f"rms_unfiltered: {rms_unfiltered}")
     print(f"diff rms: {(rms_unfiltered - rms_filtered)}")
 
+    com_ts =  np.load(f"{args.experiment_folder}/com_ts.npy")
+    com =  np.load(f"{args.experiment_folder}/com.npy")
+    com_unfiltered =  np.load(f"{args.experiment_folder}/com_unfiltered.npy")
+    cop =  np.load(f"{args.experiment_folder}/cop.npy")
+
+    # plt.plot(error_filtered,0], label="error filtered")
+    # plt.plot(error_unfiltered, label="unfliltered filtered")
+
+    plt.title("Wrist Z")
+    plt.plot(truth[:, 2, 2], label="qtm wrist z")
+    plt.plot(unfiltered[:, 2, 2], label="unfiltered wrist z")
+    plt.plot(filtered[:, 2, 2], label="filtered wrist z")
+    plt.legend()
+    plt.show()
+    plt.cla()
+
+
+    plt.plot(com_ts, com[:,0], label="com filtered")
+    plt.plot(com_ts, com_unfiltered[:,0], label="com unfiltered")
+    plt.plot(com_ts, cop[:,0], label="cop qtm")
+    plt.legend()
+    plt.show()
+    plt.cla()
+
+    plt.plot(com_ts, com[:,1], label="com filtered")
+    plt.plot(com_ts, com_unfiltered[:,1], label="com unfiltered")
+    plt.plot(com_ts, cop[:,1], label="cop qtm")
+    plt.legend()
+    plt.show()
+    plt.cla()
 
 if __name__ == "__main__":
     main()
