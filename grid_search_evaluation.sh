cc99065491411ca2602651dd6cf9f856833989ce
buermarc <buermarc2@gmail.com>
2024-04-22 18:46:24 +0200

    Cleanup

--- grid_search_evaluation.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p ./plot/results/simulation
-for i in $(python -c 'import numpy as np; [print(i) for i in np.arange(0, 0.0016, 0.00001)]'); do
-    ./build/load $i
-    cd plot
-    python simulation_evaluate.py > results/simulation/${i}.log
-    cd ..
-done
--- plot/point3d.py
+++
@@ -1,6 +0,0 @@
-import pandas
-mat = pandas.read_csv("../../matlab/file1.csv")
-mat = mat[mat.columns[17*3:18*3]]
-one = pandas.read_csv("../data/point3d.csv")
-
-breakpoint()
